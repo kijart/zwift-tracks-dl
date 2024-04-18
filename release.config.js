371fcabf29403d77d82b250fc5d9586cef163de8
Luis Miguel Vicente Fuentes <1217588+kijart@users.noreply.github.com>
2024-04-18 23:30:29 +0200

    ci(semantic-release): update configuration with commit analyzer and git plugins

--- release.config.js
+++ release.config.js
@@ -1,11 +1,13 @@
 module.exports = {
   tagFormat: '${version}',
   plugins: [
+    '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
     [
       '@semantic-release/git',
       {
-        assets: ['package.json'],
+        assets: ['package.json', 'package-lock.json'],
         message: 'chore(release): v${nextRelease.version}',
       },
     ],
